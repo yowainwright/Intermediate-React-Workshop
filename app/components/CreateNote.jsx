36510094707df02d86e62fc11202b3d0466e2b3e
Jeff Wainwright <yowainwright@gmail.com>
2018-09-30 14:01:26 -0400

    [init] setup form

--- app/components/CreateNote.jsx
+++ app/components/CreateNote.jsx
@@ -1,16 +1,52 @@
 import React, { Component } from 'react';
 import { NoteConsumer } from './NoteProvider';
+// import ToggleDrawer from './ToggleDrawer';
+import Form from './styles/Form';
+import FancyButton from './styles/FancyButton';
+import CreateNoteDropDown from './styles/CreateNoteDropdown';
 
 class CreateNote extends Component {
-  render() {
+  state = {
+    title: '',
+    content: '',
+  }
+
+  saveToState = event => this.setState({ [event.target.name]: event.target.value });
+
+  render () {
     return (
       <NoteConsumer>
-        {({ state, toggleDrawer }) => (
-          <p>I will tell you if I'm open or closed:
-            {state.drawerOpen ? 'Open!!!' : 'Closed'}
-            <button onClick={toggleDrawer}>Toggle</button>
-          </p>
-        )}
+        {({ state, toggleDrawer }) => {
+          const { content, title } = this.state;
+          return (
+            <CreateNoteDropDown open>
+              <Form>
+                <label htmlFor="title">
+                  <input
+                    value={title}
+                    onChange={this.saveToState}
+                    required
+                    type="text"
+                    name="title"
+                    id="title"
+                  />
+                </label>
+                <label htmlFor="content">
+                  <textarea
+                    value={content}
+                    onChange={this.saveToState}
+                    required
+                    type="text"
+                    name="content"
+                    id="content"
+                  />
+                </label>
+                <FancyButton type="submit">Save Note</FancyButton>
+                <FancyButton onClick={toggleDrawer}>Toggle</FancyButton>
+              </Form>
+            </CreateNoteDropDown>
+          )
+        }}
       </NoteConsumer>
     );
   }
